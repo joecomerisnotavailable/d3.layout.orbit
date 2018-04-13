96af6f9c5dded7acfd746b2bd12409a9800fd6ff
Kevinisagirl <kevin.ann.hunt@gmail.com>
2018-04-13 16:40:10 -0400

    Cleaning out moon orbit code. Changing colors.

--- Working/d3.layout.orbit.js
+++ Working/d3.layout.orbit.js
@@ -17,28 +17,12 @@ d3.layout.orbit = function() {
 
 
 	function _orbitLayout() {
-
 		return _orbitLayout;
 	}
 
 	_orbitLayout.mode = function(_mode) {
 		//Atomic, Solar, other?
 		if (!arguments.length) return orbitMode;
-
-		if (_mode == "solar") {
-			fixedOrbitArray = [1]
-		}
-		if (_mode == "atomic") {
-			fixedOrbitArray = [2,8]
-		}
-		if (_mode == "flat") {
-			fixedOrbitArray = [99]
-		}
-		orbitMode = _mode;
-		if (Array.isArray(_mode)) {
-			fixedOrbitArray = _mode;
-			orbitMode = "custom";
-		}
 		return this
 	}
 
@@ -52,10 +36,6 @@ d3.layout.orbit = function() {
 					_node.x = _node.parent.x + ( orbitRadius(_node) * Math.sin( _node.angle + (currentTickStep * tickRadianStep * tickRadianFunction(_node))) );
 					_node.y = _node.parent.y + ( orbitRadius(_node) * Math.cos( _node.angle + (currentTickStep * tickRadianStep * tickRadianFunction(_node))) );
 				}
-			})
-			orbitalRings.forEach(function(_ring) {
-				_ring.x = _ring.source.x;
-				_ring.y = _ring.source.y;
 			})
 			orbitDispatch.tick();
 		}, 
@@ -88,7 +68,7 @@ d3.layout.orbit = function() {
 	}
 
 	_orbitLayout.totallyrad = function(_function) {
-		//change ring size reduction (make that into dynamic function)
+		//our function to specify radius
 		if (!arguments.length) return orbitRadius;
 		orbitRadius = _function;
 		return this
@@ -132,24 +112,15 @@ d3.layout.orbit = function() {
 		var _data = nestedNodes; 
 	//If you have an array of elements, then create a root node (center)
 		//In the future, maybe make a binary star kind of thing?
-		if (!childrenAccessor(_data)) {
-			orbitNodes = {key: "root", values: _data}
-			childrenAccessor(orbitNodes).forEach(function (_node) {
-				_node.parent = orbitNodes;
-			})
-		}
-		//otherwise assume it is an object with a root node
-		else {
-			orbitNodes = _data;
-		}
-			orbitNodes.x = orbitSize[0] / 2;
-			orbitNodes.y = orbitSize[1] / 2;
-			orbitNodes.ring = orbitSize[0] / 2;
-			orbitNodes.depth = 0;
+		orbitNodes = {key: "root", values: _data}
+		orbitNodes.x = orbitSize[0] / 2;
+		orbitNodes.y = orbitSize[1] / 2;
+		orbitNodes.ring = orbitSize[0] / 2;
+		orbitNodes.depth = 0;
 
-			flattenedNodes.push(orbitNodes);
+		flattenedNodes.push(orbitNodes);
 
-				traverseNestedData(orbitNodes);
+		traverseNestedData(orbitNodes);
 
 		function traverseNestedData(_node) {
 			if(childrenAccessor(_node)) {
@@ -159,12 +130,7 @@ d3.layout.orbit = function() {
 				var _total_positions = 0;
 				var _p = 0;
 				while (_total_positions < totalChildren) {
-					if (fixedOrbitArray[_p]) {
-						_total_positions += fixedOrbitArray[_p];
-					}
-					else {
-						_total_positions += fixedOrbitArray[fixedOrbitArray.length - 1];						
-					}
+					_total_positions += fixedOrbitArray[_p];
 					_p++;
 					_rings++;
 				}
@@ -175,32 +141,8 @@ d3.layout.orbit = function() {
 					var _p = 0;
 					var _total_positions = 0;
 
-				while (_total_positions <= y) {
-					if (fixedOrbitArray[_p]) {
-						_total_positions += fixedOrbitArray[_p];
-					}
-					else {
-						_total_positions += fixedOrbitArray[fixedOrbitArray.length-1];						
-					}
-
-						_p++;
-						_currentRing++;
-				}
-
-				var ringSize = fixedOrbitArray[fixedOrbitArray.length-1];
-
-				if (fixedOrbitArray[_currentRing-1]) {
-					ringSize = fixedOrbitArray[_currentRing-1];
-				}
-
-					if (_node.parent) {
-						var _ring = {source: _node, x: _node.x, y: _node.y, r: _node.parent.ring / orbitDepthAdjust(_node) * (_currentRing / _rings)};
-					}
-					else {
-						var _ring = {source: _node, x: _node.x, y: _node.y, r: (orbitSize[0]/2) * (_currentRing / _rings)};
-					} // IT"S HERE!!!!!!!!!!!!!
-
-
+					var ringSize = fixedOrbitArray[fixedOrbitArray.length-1];
+					var _ring = {source: _node, x: _node.x, y: _node.y, r: (orbitSize[0]/2) * (_currentRing / _rings)};
 					var thisPie = d3.layout.pie().value(function(d) {return childrenAccessor(d) ? 4 : 1});
 					var piedValues = thisPie(childrenAccessor(_node).filter(function(d,i) {return i >= y && i <= y+ringSize-1}));
 
@@ -210,15 +152,9 @@ d3.layout.orbit = function() {
 						childrenAccessor(_node)[x].parent = _node;
 						childrenAccessor(_node)[x].depth = _node.depth + 1;
 
-						childrenAccessor(_node)[x].x = childrenAccessor(_node)[x].parent.x + ( (childrenAccessor(_node)[x].parent.ring / 2) * Math.sin( childrenAccessor(_node)[x].angle ) );
-						childrenAccessor(_node)[x].y = childrenAccessor(_node)[x].parent.y + ( (childrenAccessor(_node)[x].parent.ring / 2) * Math.cos( childrenAccessor(_node)[x].angle ) );
-
-						childrenAccessor(_node)[x].ring = _ring.r;
-
 						flattenedNodes.push(childrenAccessor(_node)[x]);
-						traverseNestedData(childrenAccessor(_node)[x]);
 					}
-					orbitalRings.push(_ring);
+
 					y+=ringSize;
 				}
 
