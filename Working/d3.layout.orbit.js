4f24fc30d2a8ae752760cb794c04d98d63dcc251
Kevinisagirl <kevin.ann.hunt@gmail.com>
2018-04-17 21:48:16 -0400

    Fully commented code. Cleaned up unused functions. Removed Dummy planet from .json.

--- Working/d3.layout.orbit.js
+++ Working/d3.layout.orbit.js
@@ -7,155 +7,119 @@ d3.layout.orbit = function() {
 	var tickRadianStep = 0.004363323129985824;
     var orbitDispatch = d3.dispatch('tick');
     var tickInterval;
-    var orbitalRings = [];
-    var orbitDepthAdjust = function() {return 2.95};
     var childrenAccessor = function(d) {return d.children};
     var tickRadianFunction = function() {return 1};
     var orbitRadius = function() {return 1};
-    var fixedOrbitArray = [99];
-    var orbitMode = "flat";
 
-
+	// returns instance of _orbitLayout
+	// useful to change layout of this instance from another file
+	// not used in this project
 	function _orbitLayout() {
 		return _orbitLayout;
 	}
 
-	_orbitLayout.mode = function(_mode) {
-		//Atomic, Solar, other?
-		if (!arguments.length) return orbitMode;
-		return this
-	}
-
+	// function to power the animation
 	_orbitLayout.start = function() {
-		//activate animation here
 		tickInterval = setInterval(
 			function() {
-			currentTickStep++;
-			flattenedNodes.forEach(function(_node){
-				if (_node.parent) {
+			currentTickStep++; // increment which tick we're on
+			flattenedNodes.forEach(function(_node){ // for each planet
+				if (_node.parent) { // if this planet has a parent (so it's not the center point)
+					// animate it spinning around the parent
+					// calculate the new x and y to draw the planet
 					_node.x = _node.parent.x + ( orbitRadius(_node) * Math.sin( _node.angle + (currentTickStep * tickRadianStep * tickRadianFunction(_node))) );
 					_node.y = _node.parent.y + ( orbitRadius(_node) * Math.cos( _node.angle + (currentTickStep * tickRadianStep * tickRadianFunction(_node))) );
 				}
 			})
-			orbitDispatch.tick();
+			orbitDispatch.tick(); // send a message to the html to redraw the planets
 		}, 
-		10);
+		1);
 	}
 
+	// function to stop the spinning
 	_orbitLayout.stop = function() {
-		//deactivate animation here
 		clearInterval(tickInterval);
 	}
 
+	// can be used to adjust the speed of all the points
 	_orbitLayout.speed = function(_degrees) {
 		if (!arguments.length) return tickRadianStep / (Math.PI / 360);
 		tickRadianStep = tickRadianStep = _degrees * (Math.PI / 360);
 		return this;
 	}
 
+	// function to set the size of the visualization
 	_orbitLayout.size = function(_value) {
 		if (!arguments.length) return orbitSize;
 		orbitSize = _value;
 		return this;
-		//change size here
 	}
 
+	// function to set the speed of the orbits
 	_orbitLayout.revolution = function(_function) {
-		//change ring size reduction (make that into dynamic function)
 		if (!arguments.length) return tickRadianFunction;
 		tickRadianFunction = _function;
 		return this
 	}
 
+	// function to set the orbit Radius of the planets
 	_orbitLayout.totallyrad = function(_function) {
-		//our function to specify radius
 		if (!arguments.length) return orbitRadius;
 		orbitRadius = _function;
 		return this
 	}
 
-	_orbitLayout.orbitSize = function(_function) {
-		//change ring size reduction (make that into dynamic function)
-		if (!arguments.length) return orbitDepthAdjust;
-		orbitDepthAdjust = _function;
-		return this
-	}
-
-	_orbitLayout.orbitalRings = function() {
-		//return an array of data corresponding to orbital rings
-		if (!arguments.length) return orbitalRings;
-		return this;
-	}
-
+	// function to transform the original data by calling calculateNodes
 	_orbitLayout.nodes = function(_data) {
     	if (!arguments.length) return flattenedNodes;
-    	nestedNodes = _data;
+		nestedNodes = _data;
+		// find children and transform the data
     	calculateNodes();
 		return this;
 	}
 
+	// function to set the function used to calculate how many children nodes there are
 	_orbitLayout.children = function(_function) {
     	if (!arguments.length) return childrenAccessor;
-    	
-    	//Probably should use d3.functor to turn a string into an object key
     	childrenAccessor = _function;
     	return this;
-
-
 	}
 
-    d3.rebind(_orbitLayout, orbitDispatch, "on");
-
+	// used to send new locations to html on tick to be redrawn
+	d3.rebind(_orbitLayout, orbitDispatch, "on");
+	
+	// return the instance of the orbit class to the html
 	return _orbitLayout;
-	function calculateNodes() {
-	    orbitalRings = [];
-		var _data = nestedNodes; 
-	//If you have an array of elements, then create a root node (center)
-		//In the future, maybe make a binary star kind of thing?
-		orbitNodes = {key: "root", values: _data}
-		orbitNodes.x = orbitSize[0] / 2;
-		orbitNodes.y = orbitSize[1] / 2;
-		orbitNodes.ring = orbitSize[0] / 2;
-		orbitNodes.depth = 0;
 
+	function calculateNodes() {
+		var _data = nestedNodes; 
+		//If you have an array of elements, then create a root node (center)
+		orbitNodes = {key: "root", values: _data}
+		orbitNodes.x = orbitSize[0] / 2; // finds the center of the visualization
+		orbitNodes.y = orbitSize[1] / 2; // finds the center of the visualization
+		orbitNodes.depth = 0; // the root node (the center) has depth 0
+
+		// add this data with a new center as the key to flattenedNodes
 		flattenedNodes.push(orbitNodes);
 
+		// sends this data to traverseNestedData
 		traverseNestedData(orbitNodes);
 
+		// for each child of the current object, we will add the starting position to be drawn,
+		// the identity of the parent, and the depth of 1 for each
 		function traverseNestedData(_node) {
-			if(childrenAccessor(_node)) {
-				var y = 0;
-				var totalChildren = childrenAccessor(_node).length;
-				var _rings = 0;
-				var _total_positions = 0;
-				var _p = 0;
-				while (_total_positions < totalChildren) {
-					_total_positions += fixedOrbitArray[_p];
-					_p++;
-					_rings++;
-				}
-
-				while (y < totalChildren) {
-					var _pos = 0;
-					var _currentRing = 0;
-					var _p = 0;
-					var _total_positions = 0;
-
-					var ringSize = fixedOrbitArray[fixedOrbitArray.length-1];
-					var _ring = {source: _node, x: _node.x, y: _node.y, r: (orbitSize[0]/2) * (_currentRing / _rings)};
-					var thisPie = d3.layout.pie().value(function(d) {return childrenAccessor(d) ? 4 : 1});
-					var piedValues = thisPie(childrenAccessor(_node).filter(function(d,i) {return i >= y && i <= y+ringSize-1}));
-
-					for (var x = y; x<y+ringSize && x<totalChildren;x++) {
-						childrenAccessor(_node)[x].angle = ((piedValues[x - y].endAngle - piedValues[x - y].startAngle) / 2) + piedValues[x - y].startAngle;
-
-						childrenAccessor(_node)[x].parent = _node;
-						childrenAccessor(_node)[x].depth = _node.depth + 1;
-
-						flattenedNodes.push(childrenAccessor(_node)[x]);
-					}
-					// orbitalRings.push(_ring);
-					y+=ringSize;
+			if(childrenAccessor(_node)) { // if there are children of this data (i.e. planets, moons)
+				var totalChildren = childrenAccessor(_node).length; // how many children
+				// for each child
+				for (var x = 0; x<totalChildren;x++) {
+					// set the random starting point
+					childrenAccessor(_node)[x].angle = Math.random()*360;
+					// set the parent of this child
+					childrenAccessor(_node)[x].parent = _node;
+					// set the depth of this child to be 1
+					childrenAccessor(_node)[x].depth = 1;
+					// add this planet to the flattenedNodes so it can be animated orbiting it's parent
+					flattenedNodes.push(childrenAccessor(_node)[x]);
 				}
 
 			}
